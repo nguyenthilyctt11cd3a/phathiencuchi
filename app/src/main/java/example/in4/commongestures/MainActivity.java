7a34a40b15065d74d799802c35f3eefdb9154684
nguyenthilyctt11cd3a <nguyenthilyctt11cd3a@gmail.com>
2019-05-14 09:59:36 +0700

    .....

---
+++ app/src/main/java/example/in4/commongestures/MainActivity.java
@@ -0,0 +1,83 @@
+package example.in4.commongestures;
+
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity
+        implements GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener{
+
+    private TextView gestureText;
+    private GestureDetectorCompat gDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gestureText = (TextView)findViewById(R.id.gestureStatusText);
+
+        this.gDetector = new GestureDetectorCompat(this,this);
+        gDetector.setOnDoubleTapListener(this);
+    }
+    public boolean onTouchEvent(MotionEvent event) {
+        this.gDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    public boolean onDown(MotionEvent event) {
+        gestureText.setText ("onDown");
+        return true;
+    }
+
+
+    public boolean onFling(MotionEvent event1, MotionEvent event2,
+                           float velocityX, float velocityY) {
+        gestureText.setText("onFling");
+        return true;
+    }
+
+    public void onLongPress(MotionEvent event) {
+        gestureText.setText("onLongPress");
+    }
+
+
+    public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                            float distanceX, float distanceY) {
+        gestureText.setText("onScroll");
+        return true;
+    }
+
+
+    public void onShowPress(MotionEvent event) {
+        gestureText.setText("onShowPress");
+    }
+
+
+    public boolean onSingleTapUp(MotionEvent event) {
+        gestureText.setText("onSingleTapUp");
+        return true;
+    }
+
+
+    public boolean onDoubleTap(MotionEvent event) {
+        gestureText.setText("onDoubleTap");
+        return true;
+    }
+
+
+    public boolean onDoubleTapEvent(MotionEvent event) {
+        gestureText.setText("onDoubleTapEvent");
+        return true;
+    }
+
+
+    public boolean onSingleTapConfirmed(MotionEvent event) {
+        gestureText.setText("onSingleTapConfirmed");
+        return true;
+    }
+}
